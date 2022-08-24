b90e5babd53d5c6597d03e51ee9ea452272cb15f
maxunspecified <jsdranony@gmail.com>
2022-08-24 19:02:27 +0530

    v-1.0.9

---
+++ README.md
@@ -0,0 +1,10 @@
+unspecmusicplaylist live-website --> 
+
+A go-to web music playlist with some built in music. If you want a better experience make sure to add your own songs to this playlist.
+
+This is a music playlist. There are already some in-built music added. So, since this is an open-source web playlist you can add you own music with image by just adding the music in the music file & the banner image in the images file.
+
+## customization
+Now to add your own music and images just changes the name of the file after the images/your-image.png (filetype) Next add the name of the music in the name field under the double inverted commas Same as that add your artist name Finally add your music file name after the music/music-name.mp3 (filetype)
+
+## screenshots
---
+++ music-list.js
@@ -0,0 +1,44 @@
+let allMusic = [
+    {
+      name: "Con Calma",
+      artist: "Daddy Yankee & Snow",
+      img: "concalma",
+      src: "concalma"
+    },
+    {
+      name: "Enemy",
+      artist: "Imagine Dragons",
+      img: "enemy",
+      src: "enemy"
+    },
+    {
+      name: "From The D 2 The LBC",
+      artist: "Eminem & Snoop Dogg",
+      img: "fromthedtothelbc",
+      src: "fromthed2thelbc"
+    },
+    {
+        name: "Faded",
+        artist: "Alan Walker",
+        img: "faded",
+        src: "faded"
+      },
+      {
+        name: "Que Tire Pa Lante",
+        artist: "Daddy Yankee",
+        img: "qtp",
+        src: "quetirepalante"
+      },
+      {
+        name: "Stay",
+        artist: "Kid Laori & Justin Bieber",
+        img: "stay",
+        src: "stay"
+      },
+      {
+        name: "Till I Collapse",
+        artist: "Eminem",
+        img: "tillicollapse",
+        src: "tillicollapse"
+      },
+];
